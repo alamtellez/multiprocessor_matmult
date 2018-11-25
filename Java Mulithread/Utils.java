8443883b86a360bed0cef47bfedd1ece4df4b1af
Arturo Tellez <A01205569@gpuserver.qro.itesm.mx>
2018-11-24 22:26:13 -0600

    Utils Java

---
+++ Java Mulithread/Utils.java
@@ -0,0 +1,23 @@
+import java.util.Random;
+
+public class Utils {
+	private static final int MAX_VALUE = 100;
+	private static final Random r = new Random();
+	
+	public static void fillMatrix(int mat[][]) {
+        Random r = new Random();
+		for (int i = 0; i < mat.length; i++) {
+            for(int j = 0; j< mat[0].length; j++)
+			    mat[i][j] = r.nextInt(MAX_VALUE) + 1;
+		}
+	}
+    public static void displayMatrix(int mat[][], char mat_name) {
+        System.out.println("Matrix " + mat_name);
+		for (int i = 0; i < mat.length; i++) {
+            for(int j = 0; j< mat[0].length; j++)
+                System.out.print(mat[i][j] + "\t");
+            System.out.print("\n");
+        }
+        System.out.println();
+	}
+}
