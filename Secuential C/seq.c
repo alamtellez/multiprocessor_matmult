031413e729a8e8b359b7a0bbadc1c4e08ae5cc7e
atellez08 <tellezarturo08@gmail.com>
2018-11-25 20:23:44 -0600

    Mults

--- Secuential C/seq.c
+++ Secuential C/seq.c
@@ -10,52 +10,50 @@
 #include <stdlib.h>
 #include "../utils/cheader.h"
 
-#define SIZE 5
 int main(int argc, char const *argv[])
 {
-    int i, j ,k, sum;
+    int i, j, k, sum;
     int filasA;
     int columnasA;
     int filasB;
     int columnasB;
     int **a, **b, **c;
     double ms;
-    
+
     srand(time(0));
-    printf ("Rows of matrix A: ");
-    scanf ("%i",&filasA);
-    printf ("Columns of matrix A: ");
-    scanf ("%i",&filasB);
+    printf("Rows of matrix A: ");
+    scanf("%i", &filasA);
+    printf("Columns of matrix A: ");
+    scanf("%i", &filasB);
     columnasA = filasB;
-    printf ("Columns of matrix B: ");
-    scanf ("%i",&columnasB);
-    
+    printf("Columns of matrix B: ");
+    scanf("%i", &columnasB);
+
     // printf("%i %i %i", filasA, columnasA, columnasB);
 
-
     // Allocate matrix A
-    a=(int **) malloc(filasA*sizeof(int *));
-    for(i=0;i < filasA ; i++)
-        a[i]=(int *) malloc(columnasA*sizeof(int));
+    a = (int **)malloc(filasA * sizeof(int *));
+    for (i = 0; i < filasA; i++)
+        a[i] = (int *)malloc(columnasA * sizeof(int));
 
     // Allocate matrix B
-    b = (int **) malloc(filasB*sizeof(int *));
+    b = (int **)malloc(filasB * sizeof(int *));
     i = 0;
-    for(i = 0;i < filasB ; i++)
-        b[i]=(int *) malloc(columnasB*sizeof(int));
+    for (i = 0; i < filasB; i++)
+        b[i] = (int *)malloc(columnasB * sizeof(int));
 
     // Allocate matrix C
-    c = (int **) malloc(filasA*sizeof(int *));
+    c = (int **)malloc(filasA * sizeof(int *));
     i = 0;
-    for(i=0;i<filasA;i++)
-        c[i]=(int *) malloc(columnasB*sizeof(int));
+    for (i = 0; i < filasA; i++)
+        c[i] = (int *)malloc(columnasB * sizeof(int));
 
     // Fill A with random values
-    for(i=0 ; i < filasA; i++)
+    for (i = 0; i < filasA; i++)
         for (j = 0; j < columnasA; j++)
             a[i][j] = (rand() % 100) + 1;
     // Display A
-    int m,n;
+    int m, n;
     // printf("\nMat A\n");
     // for (m = 0; m < filasA; m++) {
     //     for (n = 0; n < columnasA; n++){
@@ -63,7 +61,7 @@ int main(int argc, char const *argv[])
     //     }
     //     printf("\n");
     // }
-    for(i=0 ; i < filasB; i++)
+    for (i = 0; i < filasB; i++)
         for (j = 0; j < columnasB; j++)
             b[i][j] = (rand() % 100) + 1;
     // printf("\nMat B\n");
@@ -76,9 +74,12 @@ int main(int argc, char const *argv[])
     ms = 0;
     sum = 0;
     start_timer();
-    for (i = 0; i < filasA; i++) {
-        for (j = 0; j < columnasB; j++) {
-            for (k = 0; k < filasB; k++) {
+    for (i = 0; i < filasA; i++)
+    {
+        for (j = 0; j < columnasB; j++)
+        {
+            for (k = 0; k < filasB; k++)
+            {
                 sum += a[i][k] * b[k][j];
             }
             c[i][j] = sum;
@@ -102,5 +103,3 @@ int main(int argc, char const *argv[])
 
     return 0;
 }
-
-
